ac77ccec5ec49ee65912b6bf99662e2beb18bda7
AndyLin <andy.lin.hswei@gmail.com>
2023-03-30 10:32:23 +0800

    [Feature] Add `rescue` to capture Error
    `retry` when `rescue` capture Error

--- bot.rb
+++ bot.rb
@@ -4,38 +4,49 @@ require 'telegram_bot'
 ruby bot.rb
 =end
 
-# telegram bot token which @botfather generated
-botToken = ''
+begin
+    
+    # telegram bot token which @botfather generated
+    botToken = ''
 
-# Authority telegram username。 eq. @AndyL1n -> AndyL1n
-authUsers = ['']
+    # Authority telegram username。 eq. @AndyL1n -> AndyL1n
+    authUsers = ['']
 
-bot = TelegramBot.new(token: botToken)
+    bot = TelegramBot.new(token: botToken)
+    
+    puts "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}] 開始監聽"
+    
+    bot.get_updates(fail_silenty: true) do |message|
 
-bot.get_updates(fail_silenty: true) do |message|
+        puts "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}] @#{message.from.username}: #{message.text}"
+        command = message.get_command_for(bot)
 
-	puts "@#{message.from.username}: #{message.text}"
-	command = message.get_command_for(bot)
+        if authUsers.include?("#{message.from.username}")
+            message.reply do |reply|
+            case command
+            when /greeting/i
+                reply.text = "@#{message.from.username}\nHello"
+            when /cmd/i
+                # 執行 terminal 指令..
+                system("<Any thing you wanna do...>")
+                reply.text = "@#{message.from.username}\nCustom command"
+            else
+                reply.text = "@#{message.from.username}\n未知的指令: #{command.inspect}"
+            end
 
-	if authUsers.include?("#{message.from.username}")
-		message.reply do |reply|
-		case command
-		when /greeting/i
-			reply.text = "@#{message.from.username}\nHello"
-		when /cmd/i
-			# 執行 terminal 指令..
-			system("<Any thing you wanna do...>")
-			reply.text = "@#{message.from.username}\nCustom command"
-		else
-			reply.text = "@#{message.from.username}\n未知的指令: #{command.inspect}"
-		end
-
-		puts "sending #{reply.text.inspect} to @#{message.from.username}"
-		reply.send_with(bot)
-		end
-	else
-		bot.send_message(chat_id: message.chat.id, text: "@#{message.from.username} 沒有權限")
-		puts "@#{message.from.username} 沒有權限"
-	end
-	
+            puts "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}] sending #{reply.text.inspect} to @#{message.from.username}"
+            reply.send_with(bot)
+            end
+        else
+            bot.send_message(chat_id: message.chat.id, text: "@#{message.from.username} 沒有權限")
+            puts "@#{message.from.username} 沒有權限"
+        end
+        
+    end
+    
+rescue
+    puts "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}] 發生錯誤，執行 retry"
+    retry
+    
 end
+
